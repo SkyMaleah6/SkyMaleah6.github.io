231be1ea51d720c63499643bc7704bbebd6222f7
SkyMaleah6 <sky.hughes@inspire35.org>
2026-05-11 20:32:33 +0000

    I'm Finished

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -1,9 +1,9 @@
 // This is a small program. There are only two sections. This first section is what runs
 // as soon as the page loads.
 $(document).ready(function () {
-  render($("#display"), image);
-  $("#apply").on("click", applyAndRender);
-  $("#reset").on("click", resetAndRender);
+render($("#display"), image);
+$("#apply").on("click", applyAndRender);
+$("#reset").on("click", resetAndRender);
 });
 
 /////////////////////////////////////////////////////////
@@ -12,63 +12,51 @@ $(document).ready(function () {
 
 // this function resets the image to its original value; do not change this function
 function resetAndRender() {
-  reset();
-  render($("#display"), image);
+reset();
+render($("#display"), image);
 }
 
 // this function applies the filters to the image and is where you should call
 // all of your apply functions
 function applyAndRender() {
-  // Multiple TODOs: Call your apply function(s) here
- applyFilter(reddify);
-
- 
- //applyFilterNoBackground(increaseGreenByBlue);
- //applyFilter(increaseGreenByBlue)
-
- applyFilterNoBackground(reddify);
-
-
+// Multiple TODOs: Call your apply function(s) here
+applyFilter(reddify);
 applyFilterNoBackground(decreaseBlue);
-applyFilter(decreaseBlue);
- 
+applyFilter(increaseGreenByBlue);
+applyFilterNoBackground(reddify);
+
 // do not change the below line of code
-  render($("#display"), image);
+render($("#display"), image);
 }
+
 /////////////////////////////////////////////////////////
 // "apply" and "filter" functions should go below here //
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
-function applyFilter(filterFunction){
-for (var r = 0; r < image.length; r++){
-for (var c = 0; c < image[r].length; c++){
-var pixel = image[r][c];
+function applyFilter(filterFunction) {
+for (var i = 0; i < image.length; i++) {
+for (var j = 0; j < image[i].length; j++) {
+var pixel = image[i][j];
 var pixelArray = rgbStringToArray(pixel);
+
 filterFunction(pixelArray);
 
-
 var updatedPixel = rgbArrayToString(pixelArray);
-image[r][c] = updatedPixel;
-//This where I'll modify the color values later
-
-
-
+image[i][j] = updatedPixel;
 }
 }
 }
-
 
 // TODO 9 Create the applyFilterNoBackground function
 function applyFilterNoBackground(filterFunction) { // Added { here
 var backgroundColor = image[0][0];
-for (var r = 0; r < image.length; r++) {
-for (var c = 0; j < image[r].length; c++) {
-if (image[r][c] !== backgroundColor) {
-  var pixel = image[r][c];
-var pixelArray = rgbStringToArray(pixel);
+for (var i = 0; i < image.length; i++) {
+for (var j = 0; j < image[i].length; j++) {
+if (image[i][j] !== backgroundColor) {
+var pixelArray = rgbStringToArray(image[i][j]);
 filterFunction(pixelArray);
-image[r][c] = rgbArrayToString(pixelArray);
+image[i][j] = rgbArrayToString(pixelArray);
 }
 }
 }
@@ -77,30 +65,21 @@ image[r][c] = rgbArrayToString(pixelArray);
 
 // TODO 6: Create the keepInBounds function
 function keepInBounds(num) {
-if(num < 0){
-  return 0;
-}
-else if(num > 225){
-  return 225;
-}
-else{
-  return num;
-}
+return num < 0 ? 0 : (num > 255 ? 255 : num);
 }
 
 // TODO 4: Create reddify filter function
-function reddify(array){
-array[RED] = 200;
+function reddify(pixelArray) {
+pixelArray[RED] = 200;
 }
-/*var testArray = [100, 100, 100];
-reddify(testArray);
-console.log(testArray); // Should show [200, 100, 100]*/
+
 // TODO 7 & 8: Create more filter functions
-function decreaseBlue(array) {
-array[BLUE] -= 50;
-array = keepInBounds(array[BLUE]);
+function decreaseBlue(pixelArray) {
+var newBlue = pixelArray[BLUE] - 50;
+pixelArray[BLUE] = keepInBounds(newBlue);
 }
-function increaseGreenByBlue(array){
-  array[GREEN] = keepInBounds(array[GREEN] + array[BLUE]);
+
+function increaseGreenByBlue(pixelArray) {
+var newGreen = pixelArray[GREEN] + pixelArray[BLUE];
+pixelArray[GREEN] = keepInBounds(newGreen);
 }
-// CHALLENGE code goes below here
